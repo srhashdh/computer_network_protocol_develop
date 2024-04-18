2d8448a5f080ea819c8f7865c382ac92386bf42b
srhashdh <together219@gmail.com>
2024-04-18 17:06:54 +0800

    conn_02_01

--- weather.c
+++ weather.c
@@ -16,20 +16,20 @@ char *arr[] = {
     "Please guess: ZmxhZ3tzYW5kX3N0MHJtfQ=="
 };
 void weather(const char *city, int sockfd){
+    char number;
     while(1){
-        char *number[20];
         printf("Please enter the given number to query\n");
         printf("1.today\n");
         printf("2.three days from today\n");
         printf("3.custom day by yourself\n");
         printf("(r)back,(c)cls,(#)exit\n");
         printf("==========================================================\n");
-        scanf("%d", number);
+        scanf("%c", number);
         bool flag = false;
         while(!flag){
             time_t t = time(NULL);
             struct tm *now = localtime(&t);
-            if(strcmp(number, "1") == 0){
+            if(strcmp(number, '1') == 0){
                 char w;
                 char t;
                 conn_02_01(sockfd, city, 1);
@@ -41,13 +41,13 @@ void weather(const char *city, int sockfd){
                 printf("City: %s  Today is: %d/%02d/%02d Weather information is as follow:\n", city, now->tm_year + 1900, now->tm_mon + 1, now->tm_mday);
                 printf("Today's Weather is: %s, Temp:%d\n", arr[w_entry], t_value);
             }
-            else if(strcmp(number, "2") == 0){
+            else if(strcmp(number, '2') == 0){
                 printf("City: %s  Today is: %d/%02d/%02d Weather information is as follow:\n", city, now->tm_year + 1900, now->tm_mon + 1, now->tm_mday);
                 printf("The 1th day's Weather is: \n");
                 printf("The 2th day's Weather is: \n");
                 printf("The 3th day's Weather is: \n");
             }
-            else if(strcmp(number, "3") == 0){
+            else if(strcmp(number, '3') == 0){
                 printf("Please enter the day number(below 10, e.g. 1 means today):");
                 bool flag2 = false;
                 while(!flag2){
@@ -69,14 +69,14 @@ void weather(const char *city, int sockfd){
                     }
                 }
             }
-            else if(strcmp(number, "r") == 0){
+            else if(strcmp(number, 'r') == 0){
                 main();
             }
-            else if(strcmp(number, "c") == 0){
+            else if(strcmp(number, 'c') == 0){
                 system("clear");
                 break;
             }
-            else if(strcmp(number, "#") == 0){
+            else if(strcmp(number, '#') == 0){
                 exit(0);
             }
             else{
