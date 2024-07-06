cea7f60f3fea981d75461836d06ef693e8c6937c
srhashdh <together219@gmail.com>
2024-07-06 15:00:43 +0800

    ip change socket change array

--- lab4/client.c
+++ lab4/client.c
@@ -6,11 +6,11 @@
 #include <sys/socket.h>
 
 #define PORT 8080
-#define SERVER_IP "127.0.0.1"
+#define SERVER_IP "192.168.8.159"
 #define TOTAL_CONNECTIONS 1000000
 
 int main(){
-	int sock;
+	int *sock = (int *)malloc(sizeof(int) * TOTAL_CONNECTIONS);
 	struct sockaddr_in serv_addr;
 	serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
@@ -18,13 +18,13 @@ int main(){
 
 
 	for(int i = 0; i < TOTAL_CONNECTIONS; i++){
-		sock = socket(AF_INET, SOCK_STREAM, 0);
-		if(sock == -1){
+		sock[i] = socket(AF_INET, SOCK_STREAM, 0);
+		if(sock[i] == -1){
 			printf("\n Socket creation error %d\n", i);
 			return -1;
 		}
 		
-		if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1){
+		if(connect(sock[i], (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1){
 			printf("\nConnection Failed\n");
 			return -1;
 		}
